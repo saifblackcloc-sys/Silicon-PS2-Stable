4e9f9d7c142cb1ccf252d3b494a3bc716cf10e5a
saifblackcloc-sys <saifblackcloc@gmail.com>
2026-04-26 20:38:49 +0000

    Force fix: Adding M4 build system

---
+++ cpp/ios_main.h
@@ -0,0 +1,9 @@
+#ifndef ios_main_h
+#define ios_main_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+#endif /* ios_main_h */
